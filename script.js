21e9dac08156fdce6f8b99616a36b02861da25cd
HenrryVasconcelos <henrrydecannos@gmail.com>
2025-07-02 23:22:40 -0300

    mais imagens na galeria

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const imagens = [ // lista de imagens
     'img3.jpg',
     'img4.jpg',
     'img5.jpg',
-    'img6.jpg'
+    'img6.jpg',
+    'img7.PNG',
+    'img8.jpg',
+    'img9.jpg',
+    'img10.jpg',
+    'img11.jpg',
+    'img12.png'
 ];
 
 const mensagens = [ // lista de frases carinhosas
